f6ca2e31d7a460d1df703db025c70bd8f0ed056f
Chawalit Sengsang <chawalit1987@gmail.com>
2020-08-11 14:37:30 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'IndexHome',
+    redirect: { name: 'Home' },
+    component: () => import('@/views/Home.vue'),
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+  linkActiveClass: "active",
+  linkExactActiveClass: "exact-active",
+})
+
+export default router
